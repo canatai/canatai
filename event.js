0bcd43b0291931cd4771cedc075f31b7de3593a3
Frank Ou <37247299+Ou-sy@users.noreply.github.com>
2024-07-11 13:13:58 -0400

    Update event.js
    Add event

--- event.js
+++ event.js
@@ -8,5 +8,13 @@ const events = [
         time: "20:00",
         timezone: "America/Toronto"
     },
+    {
+        name: "CTCA 如何在加拿大找到軟體工程師職位工作？",
+        url: "https://streamyard.com/watch/p3TBPayfCMFU",
+        location: "Online",
+        date: "2024-07-21",
+        time: "09:00",
+        timezone: "America/Toronto"
+    },
     // Add more events as needed
 ];
